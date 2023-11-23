212153955d05977aa493b89843f03cef75344850
Aditria Pardana <aditria.pardana20@gmail.com>
2023-11-23 11:50:45 +0700

    first init

---
+++ src/app/layout.js
@@ -0,0 +1,20 @@
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata = {
+  title: "Goblogs | The best blog app",
+  description: "The best blog app!",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
