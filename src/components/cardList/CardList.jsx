631792d3bfa0bd4d10568ae041ce0293baf771b6
Aditria Pardana <aditria.pardana20@gmail.com>
2023-11-23 14:16:43 +0700

    update navbar styles

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import style from './cardList.module.css'
+import React from "react";
+import style from "./cardList.module.css";
+import Pagination from "../pagination/Pagination";
 
 const CardList = () => {
   return (
-    <div className={style.container}>CardList</div>
-  )
-}
+    <div className={style.container}>
+      CardList
+      <Pagination />
+    </div>
+  );
+};
 
-export default CardList
+export default CardList;
---
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './themeToggle.module.css'
+
+const ThemeToggle = () => {
+  return (
+    <div className={style.container}>ThemeToggle</div>
+  )
+}
+
+export default ThemeToggle
