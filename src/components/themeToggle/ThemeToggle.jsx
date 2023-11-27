299d6b86594e0d33b6e44ba427cdb42426299442
Aditria Pardana <aditria.pardana20@gmail.com>
2023-11-27 14:11:47 +0700

    update toggle styles

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import styles from './themeToggle.module.css'
+import React from "react";
+import styles from "./themeToggle.module.css";
+import Image from "next/image";
 
 const ThemeToggle = () => {
   return (
-    <div className={styles.container}>ThemeToggle</div>
-  )
-}
+    <div className={styles.container}>
+      <Image src="/moon.png" alt="moon" width={14} height={14} />
+      <div className={styles.ball}></div>
+      <Image src="/sun.png" alt="sun" width={14} height={14} />
+    </div>
+  );
+};
 
-export default ThemeToggle
+export default ThemeToggle;
