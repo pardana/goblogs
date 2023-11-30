6b45fc89fde737d4713df243bb31919805869ffb
Aditria Pardana <aditria.pardana20@gmail.com>
2023-11-30 09:08:36 +0700

    update section featured

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import style from './featured.module.css'
+import React from "react";
+import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
   return (
-    <div className={style.container}>Featured</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Aditria here!</b> Discover my stories and creative ideas.
+      </h1>
 
-export default Featured
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="post" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Loream ipsum dolor sit amet alim</h1>
+          <p className={styles.postDesc}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            unknown printer took a galley of type and scrambled it to make a
+            type specimen book. It has survived not only five centuries, but
+            also the leap into electronic typesetting, remaining essentially
+            unchanged.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
