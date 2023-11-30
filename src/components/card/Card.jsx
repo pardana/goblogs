b9fcf441578052fa69a7483ef26e8f9b887ce4cc
Aditria Pardana <aditria.pardana20@gmail.com>
2023-11-30 13:41:07 +0700

    add sectgion card

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./Card.module.css";
+
+const Card = () => {
+  return (
+    <div className={style.container}>
+      Card
+      
+
+    </div>
+  );
+};
+
+export default Card;
