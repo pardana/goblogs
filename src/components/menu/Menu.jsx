7e4d1e53a4945cb67cf91d3487f847eb20076c00
Aditria Pardana <aditria.pardana20@gmail.com>
2023-11-30 15:28:58 +0700

    update section menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,88 +1,25 @@
 import React from "react";
 import styles from "./menu.module.css";
-import Link from "next/link";
-import Image from "next/image";
+import MenuPosts from "../menuPosts/menuPosts";
+import MenuCategories from "../menuCategories/MenuCategories";
 
 const Menu = () => {
   return (
     <div className={styles.container}>
+      {/* MOST POPULAR */}
       <h2 className={styles.subtitle}>{"What's hot"}</h2>
       <h1 className={styles.title}>Most Popular</h1>
-      <div className={styles.items}>
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-          </div>
+      <MenuPosts withImage={false} />
 
-          <div className={styles.textContainer}>
-            <span className={`${styles.category} ${styles.travel}`}>
-              Travel
-            </span>
-            <h3 className={styles.postTitle}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>John Doe</span>
-              <span className={styles.date}> - 10.03.2023</span>
-            </div>
-          </div>
-        </Link>
+      {/* CATEGORIES */}
+      <h2 className={styles.subtitle}>Discover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <MenuCategories />
 
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-          </div>
-
-          <div className={styles.textContainer}>
-            <span className={`${styles.category} ${styles.culture}`}>
-              Culture
-            </span>
-            <h3 className={styles.postTitle}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>John Doe</span>
-              <span className={styles.date}> - 10.03.2023</span>
-            </div>
-          </div>
-        </Link>
-
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-          </div>
-
-          <div className={styles.textContainer}>
-            <span className={`${styles.category} ${styles.food}`}>Food</span>
-            <h3 className={styles.postTitle}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>John Doe</span>
-              <span className={styles.date}> - 10.03.2023</span>
-            </div>
-          </div>
-        </Link>
-
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-          </div>
-
-          <div className={styles.textContainer}>
-            <span className={`${styles.category} ${styles.fashion}`}>
-              Fashion
-            </span>
-            <h3 className={styles.postTitle}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>John Doe</span>
-              <span className={styles.date}> - 10.03.2023</span>
-            </div>
-          </div>
-        </Link>
-      </div>
+      {/* EDITOR PICK */}
+      <h2 className={styles.subtitle}>{"Chosen by thee editor"}</h2>
+      <h1 className={styles.title}>Editor Pick</h1>
+      <MenuPosts withImage={true} />
     </div>
   );
 };
