073f474a53c3d2878cfd46c16855a42c6a578693
Aditria Pardana <aditria.pardana20@gmail.com>
2023-12-04 10:36:38 +0700

    add repo singlePage

---
+++ src/app/[slug]/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './singlePage.module.css'
+
+const singlePage = () => {
+  return (
+    <div className={styles.container}>singlePage</div>
+  )
+}
+
+export default singlePage
