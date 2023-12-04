c1cc37081f2347d6d71ef07d71c3fb59e075f506
Aditria Pardana <aditria.pardana20@gmail.com>
2023-12-04 10:00:05 +0700

    update section footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
-import style from './footer.module.css'
+import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className={style.container}>Footer</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.png" alt="Aditria Pardana" width={50} height={50} />
+          <h1 className={styles.logoText}>GoBlogs</h1>
+        </div>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate id
+          doloremque aperiam est explicabo, harum obcaecati necessitatibus,
+          optio debitis modi earum vero facilis voluptas voluptatibus, magnam
+          velit assumenda minima saepe!
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="logo" width={18} height={18} />
+          <Image src="/instagram.png" alt="logo" width={18} height={18} />
+          <Image src="/tiktok.png" alt="logo" width={18} height={18} />
+          <Image src="/youtube.png" alt="logo" width={18} height={18} />
+        </div>
+      </div>
 
-export default Footer
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Home</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
