8bd64edb64fe3e43cb98d28a08b235ad3c37ac9f
Aditria Pardana <aditria.pardana20@gmail.com>
2023-12-04 13:27:12 +0700

    add repo singlePage

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
-import styles from './singlePage.module.css'
+import React from "react";
+import styles from "./singlePage.module.css";
+import Image from "next/image";
+import Menu from "@/components/menu/Menu";
 
 const singlePage = () => {
   return (
-    <div className={styles.container}>singlePage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <div className={styles.user}>
+            <div className={styles.userImageContainer}>
+              <Image src="/p1.jpeg" alt="img" fill className={styles.avatar} />
+            </div>
+            <div className={styles.userTextContainer}>
+              <span className={styles.username}>John Doe</span>
+              <span className={styles.date}>04.12.2023</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt="img" fill className={styles.image} />
+        </div>
+      </div>
 
-export default singlePage
+      <div className={styles.content}>
+        <div className={styles.post}>
+          <div className={styles.description}>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+              iusto quae, doloribus voluptatem corporis vel? Nostrum vitae
+              libero omnis reprehenderit animi, nam eum asperiores! Officia aut
+              optio aspernatur exercitationem quod?
+            </p>
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+              iusto quae, doloribus voluptatem corporis vel? Nostrum vitae
+              libero omnis reprehenderit animi, nam eum asperiores! Officia aut
+              optio aspernatur exercitationem quod?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+              iusto quae, doloribus voluptatem corporis vel? Nostrum vitae
+              libero omnis reprehenderit animi, nam eum asperiores! Officia aut
+              optio aspernatur exercitationem quod
+            </p>
+          </div>
+        </div>
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default singlePage;
