c7629c0b2202296c957d8a1066f5d37424958fdb
Aditria Pardana <aditria.pardana20@gmail.com>
2023-12-07 11:27:14 +0700

    update auth googlee

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,16 +1,16 @@
 "use client";
-
-import React, { useState } from "react";
-import styles from "./authLinks.module.css";
 import Link from "next/link";
+import styles from "./authLinks.module.css";
+import { useState } from "react";
+import { signOut, useSession } from "next-auth/react";
 
 const AuthLinks = () => {
-  const status = "notauthenticated";
   const [open, setOpen] = useState(false);
+  const { status } = useSession();
 
   return (
     <>
-      {status === "notauthenticated" ? (
+      {status === "unauthenticated" ? (
         <Link href="/login" className={styles.link}>
           Login
         </Link>
@@ -19,22 +19,21 @@ const AuthLinks = () => {
           <Link href="/write" className={styles.link}>
             Write
           </Link>
-          <span className={styles.link}>Logout</span>
+          <span className={styles.link} onClick={signOut}>
+            Logout
+          </span>
         </>
       )}
-
       <div className={styles.burger} onClick={() => setOpen(!open)}>
         <div className={styles.line}></div>
         <div className={styles.line}></div>
         <div className={styles.line}></div>
       </div>
-
       {open && (
         <div className={styles.responsiveMenu}>
-          <Link href="/">Home</Link>
-          <Link href="/contact">Contact</Link>
-          <Link href="/about">About</Link>
-
+          <Link href="/">Homepage</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
           {status === "notauthenticated" ? (
             <Link href="/login">Login</Link>
           ) : (
